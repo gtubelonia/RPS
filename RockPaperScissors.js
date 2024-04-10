f84a08052f3a506af029c809a3a874898426d6f5
Gian <gtubelonia@live.com>
2024-04-10 15:20:14 -0500

    misread the assignment. added win trackers

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -11,40 +11,55 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection.toUpperCase() == playerSelection.toUpperCase()) {
         result = `TIE; ${computerSelection} ties with ${playerSelection}`;
         console.log(result);
-        return;
+        return result;
     }
 
     switch (playerSelection.toUpperCase()) {
         case "ROCK":
-            if(computerSelection == "PAPER"){
-                result =  "You Lose! Paper beats Rock";
-            }else{
-                result =  "You Win! Scissors loses to Rock";
+            if (computerSelection == "PAPER") {
+                result = "You Lose! Paper beats Rock";
+            } else {
+                result = "You Win! Scissors loses to Rock";
             }
             break;
         case "PAPER":
-            if(computerSelection == "SCISSORS"){
-                result =  "You Lose! Scissors beats Paper";
-            }else{
-                result =  "You Win! Rock loses to Scissors";
+            if (computerSelection == "SCISSORS") {
+                result = "You Lose! Scissors beats Paper";
+            } else {
+                result = "You Win! Rock loses to Scissors";
             }
             break;
         case "SCISSORS":
-            if(computerSelection == "ROCK"){
-                result =  "You Lose! Rock beats Scissors";
-            }else{
-                result =  "You Win! Paper loses to Scissors";
+            if (computerSelection == "ROCK") {
+                result = "You Lose! Rock beats Scissors";
+            } else {
+                result = "You Win! Paper loses to Scissors";
             }
             break;
     }
-        console.log(result);
+
+    console.log(result);
+    return result;
 }
 
-function playGame(){
+function playGame() {
+    let playerWinCounter = 0,
+    botWinCounter = 0;
 
-    for(let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
         var playerSelection = prompt("choose Rock, Paper, or Scissors!");
         var computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        if (result.includes("You Win")) {
+            playerWinCounter += 1;
+        }else if (result.includes("You Lose")){
+            botWinCounter += 1;
+        }
+    }
+
+    if (playerWinCounter > botWinCounter) {
+        console.log("Player Wins");
+    } else {
+        console.log("Bot wins");
     }
 }
